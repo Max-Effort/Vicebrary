43b96158a2558cb5a5eb264d1a829431dc414374
Quanisha Jackson <jquanisha0031@gmail.com>
2021-09-13 16:02:51 -0500

    Editing pages and added views

---
+++ client/src/components/Views/Home.js
@@ -0,0 +1,5 @@
+export default function Home(){
+    return (<div className="renderHome">
+        <h2>This is the Home Screen</h2>
+    </div>)
+}
---
+++ client/src/components/Views/index.js
@@ -0,0 +1,25 @@
+import Add from './Add'
+import Search from './Search'
+import Home from './Home'
+import Library from './Library'
+
+export default function View(page) {
+      let view = page.page;
+// Get State from Footer through Main and render appropriate component  
+    if (view === "home") {
+      return <Home />;
+    }
+    if (view === "library") {
+      return <Library />;
+    }
+    if (view === "search") {
+      return <Search />;
+    }
+    if (view === "add") {
+      return <Add />;
+    }
+
+   
+        
+
+}
