9044b75f6c7fdebdb881bfd73d78481b1a0c42d7
Ryan Hillier <ryan1992186@gmail.com>
2021-09-14 21:41:33 -0500

    brokencode

---
+++ client/src/components/LibraryList/LibraryList.js
@@ -0,0 +1,12 @@
+// import { useEffect, useState } from 'react'
+// import { useQuery, useMutation } from '@apollo/react-hooks'
+// import { QUERY_SELF } from '../../utils/queries'
+// import Auth from '../../utils/auth'
+
+
+const LibraryList = async (userData) => {
+     console.dir({ userData })
+    return (<p> This is a p Tag </p>)
+}
+
+export default LibraryList;
