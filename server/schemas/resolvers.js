bbe002dc932086d4dc7de5c6f94da31c01b54546
Peter Roto <peterdroto@gmail.com>
2021-09-14 22:50:05 -0500

    modified resolvers to use context.user.username . . . i think

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,8 +8,8 @@ const resolvers = {
     Query: {
         // Returns All SELF(user data) if token matches
         Self: async(parent, args, context) => {
-            // console.dir({ context })
-            // if (context.user) {
+            console.dir({ context })
+                // if (context.user) {
             const userData = await db.User.findOne({ username: args.username })
                 .populate('items')
                 .populate('notes')
@@ -84,19 +84,19 @@ const resolvers = {
             }
         },
         saveItem: async(parent, args, context) => {
-            let OWNER = '61390911bfc660880c854bec'
-                // if (context.user) {
-            const savedItem = await db.Item.findOne({ _id: args.item_id })
-            if (!savedItem) {
-                const newItem = await db.Item.create({ name: args.name, vice_id: args.vice_id, vice_type: args.vice_type })
-                await db.User.findOneAndUpdate({ _id: OWNER }, { $addToSet: { items: newItem._id } })
-                return newItem;
+            // let OWNER = '61390911bfc660880c854bec' //! SWITCH FOR USING CONTEXT FROM FRONT END
+            if (context.user) {
+                const savedItem = await db.Item.findOne({ _id: args.item_id })
+                if (!savedItem) {
+                    const newItem = await db.Item.create({ name: args.name, vice_id: args.vice_id, vice_type: args.vice_type })
+                    await db.User.findOneAndUpdate({ _id: /*OWNER*/ context.user._id }, { $addToSet: { items: newItem._id } })
+                    return newItem;
+                }
+                await db.User.findOneAndUpdate({ _id: args.owner_id }, { $addToSet: { items: savedItem._id } })
+                return savedItem;
             }
-            await db.User.findOneAndUpdate({ _id: args.owner_id }, { $addToSet: { items: savedItem._id } })
-            return savedItem;
-            // }
 
-            // throw new AuthenticationError('You need to be logged in!');
+            throw new AuthenticationError('You need to be logged in!');
         },
         saveWine: async(parent, args, context) => {
             let savedWine
