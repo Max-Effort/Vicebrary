413e984f70e0cfca0649dd186fb59ced2f3ba059
Jerry Plotke <jerry.plotke@gmail.com>
2021-09-15 15:25:30 -0500

    started add wine page

--- client/src/components/Views/Add.js
+++ client/src/components/Views/Add.js
@@ -1,5 +1,33 @@
+import React from 'react';
+import { Container } from '@material-ui/core'
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
+
 export default function Add(){
-    return (<div className="renderAdd">
-        <h2>This is the Add New Wine Page</h2>
-    </div>)
+    const classes = useStyles();
+
+    return (
+        <div className="renderAdd">
+            <Container className = "addWineContainer">
+                <div>
+                    <h2>Feel Free to Add A New Wine We Have'nt Heard Of!</h2>
+                        <form className={classes.root} noValidate autoComplete="off">
+                            <TextField id="outlined-basic" label="Name" variant="outlined" />
+                            <TextField id="outlined-basic" label="Year" variant="outlined" />
+                            <TextField id="outlined-basic" label="Country" variant="outlined" />
+                            <TextField id="outlined-basic" label="Type" variant="outlined" />
+                        </form>
+                </div>
+            </Container>
+        </div>
+    )
 }
