5dcf6eacf5350948a3d7f666ea92386c344ac9c1
Quanisha Jackson <jquanisha0031@gmail.com>
2021-09-18 08:44:55 -0500

    fixed connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,12 @@
-   const mongoose = require('mongoose');
-   require('dotenv').config();
-   //    || 'mongodb://localhost/vicebrary'
-   mongoose.connect(process.env.MONGODB_URI, {
-       useNewUrlParser: true,
-       useUnifiedTopology: true,
-       //   useCreateIndex: true,
-       //   useFindAndModify: false,
-   });
+const mongoose = require('mongoose');
+require('dotenv').config();
+//    
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/vicebrary', {
 
-   module.exports = mongoose.connection;
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //   useCreateIndex: true,
+    //   useFindAndModify: false,
+});
+
+module.exports = mongoose.connection;
