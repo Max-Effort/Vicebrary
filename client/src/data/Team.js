232bdbf152985c4106a89ed4c49a114e5ec02624
Ryan Hillier <ryan1992186@gmail.com>
2021-09-18 09:50:32 -0500

    updated bio

--- client/src/data/Team.js
+++ client/src/data/Team.js
@@ -1,47 +1,47 @@
 export const Team = [{
-        imgsrc: 'https://avatars.githubusercontent.com/u/80749034?v=4',
-        name: 'Quanisha Jackson',
-        skills: 'UI/UX Design | Charisma',
-        github: 'jquanisha',
-        linkedin: 'quanisha-jackson-543901ba',
-        email: 'jquanisha0031@gmail.com',
-        bio: 'Quanisha is the best.'
-    },
-    {
-        imgsrc: 'https://raw.githubusercontent.com/Max-Effort/Vicebrary/main/client/src/components/images/nakshab.jpeg',
-        name: 'Nakshab Nadeem',
-        skills: 'Management (Maybe?)',
-        github: 'nnadeem75',
-        linkedin: 'nakshab',
-        email: 'nakshab@gmail.com',
-        bio: 'Nakshab is nice.'
-    },
-    {
-        imgsrc: 'https://raw.githubusercontent.com/Max-Effort/Vicebrary/main/client/src/components/images/ryan.jpeg',
-        name: 'Ryan Hillier',
-        skills: 'Making JSON Files',
-        github: 'Ryan1992186',
-        linkedin: 'Ryan-hillier-782216167',
-        email: 'Ryan1992186@gmail.com',
-        bio: 'This is a quick bio about Ryan. . . that was never finished.'
-    },
-    {
-        imgsrc: 'https://rb.gy/jzbfvm',
-        name: 'Peter Roto',
-        skills: 'Project Management | Server-Client Synergy',
-        github: 'PRoto133',
-        linkedin: 'peterroto',
-        email: 'peter@peterroto.com',
-        bio: 'Peter is new to the web development. He enjoys learning and using his ever growing skill-set to create things to help others. He is always open to feedback as well as new friends, so if you want to connect. . . do it.'
+    imgsrc: 'https://avatars.githubusercontent.com/u/80749034?v=4',
+    name: 'Quanisha Jackson',
+    skills: 'UI/UX Design | Charisma',
+    github: 'jquanisha',
+    linkedin: 'quanisha-jackson-543901ba',
+    email: 'jquanisha0031@gmail.com',
+    bio: 'Quanisha is the best.'
+},
+{
+    imgsrc: 'https://raw.githubusercontent.com/Max-Effort/Vicebrary/main/client/src/components/images/nakshab.jpeg',
+    name: 'Nakshab Nadeem',
+    skills: 'Management (Maybe?)',
+    github: 'nnadeem75',
+    linkedin: 'nakshab',
+    email: 'nakshab@gmail.com',
+    bio: 'Nakshab is nice.'
+},
+{
+    imgsrc: 'https://raw.githubusercontent.com/Max-Effort/Vicebrary/main/client/src/components/images/ryan.jpeg',
+    name: 'Ryan Hillier',
+    skills: 'Making JSON Files',
+    github: 'Ryan1992186',
+    linkedin: 'Ryan-hillier-782216167',
+    email: 'Ryan1992186@gmail.com',
+    bio: 'This is Ryan, he like to break things, but is also nice.'
+},
+{
+    imgsrc: 'https://rb.gy/jzbfvm',
+    name: 'Peter Roto',
+    skills: 'Project Management | Server-Client Synergy',
+    github: 'PRoto133',
+    linkedin: 'peterroto',
+    email: 'peter@peterroto.com',
+    bio: 'Peter is new to the web development. He enjoys learning and using his ever growing skill-set to create things to help others. He is always open to feedback as well as new friends, so if you want to connect. . . do it.'
 
-    },
-    {
-        imgsrc: 'https://avatars.githubusercontent.com/u/80848115?v=4',
-        name: 'Gerald Plotke',
-        skills: 'HTML, CSS, JavaScript basics, Back-end basics',
-        github: 'Gplotke',
-        linkedin: 'gerald-plotke',
-        email: 'Jerry.Plotke@gmail.com',
-        bio: `Gerald is a new web developer, (recently graduated from Northwestern's Coding Bootcamp). Additionally, he sports a Bachelor's degree (Marketing) from Illinois State University and over 7 years of sales experience in the logistics industry.`
-    }
+},
+{
+    imgsrc: 'https://avatars.githubusercontent.com/u/80848115?v=4',
+    name: 'Gerald Plotke',
+    skills: 'HTML, CSS, JavaScript basics, Back-end basics',
+    github: 'Gplotke',
+    linkedin: 'gerald-plotke',
+    email: 'Jerry.Plotke@gmail.com',
+    bio: `Gerald is a new web developer, (recently graduated from Northwestern's Coding Bootcamp). Additionally, he sports a Bachelor's degree (Marketing) from Illinois State University and over 7 years of sales experience in the logistics industry.`
+}
 ]
